81c74363fdbd6dcd17eba9d2f03f13f335d91b0e
seojinwoong <tjwlsdnd12@gmail.com>
2024-07-27 23:21:07 +0900

    first commit

---
+++ apple-clone-v11/js/main.js
@@ -0,0 +1,63 @@
+(() => {
+
+	let yOffset = 0; // window.pageYOffset 대신 쓸 변수
+	const sceneInfo = [
+		{
+			// 0
+			type: 'sticky',
+			heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+			scrollHeight: 0,
+			objs: {
+				container: document.querySelector('#scroll-section-0')
+			}
+		},
+		{
+			// 1
+			type: 'normal',
+			heightNum: 5,
+			scrollHeight: 0,
+			objs: {
+				container: document.querySelector('#scroll-section-1')
+			}
+		},
+		{
+			// 2
+			type: 'sticky',
+			heightNum: 5,
+			scrollHeight: 0,
+			objs: {
+				container: document.querySelector('#scroll-section-2')
+			}
+		},
+		{
+			// 3
+			type: 'sticky',
+			heightNum: 5,
+			scrollHeight: 0,
+			objs: {
+				container: document.querySelector('#scroll-section-3')
+			}
+		},
+	];
+
+	function setLayout() { // 각 스크롤 섹션의 높이 세팅
+		for (let i = 0; i < sceneInfo.length; i++) {
+			sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+			sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+		}
+	}
+
+	function scrollLoop() {
+		yOffset = window.pageYOffset;
+		console.log(yOffset);
+	}
+
+	setLayout();
+	window.addEventListener('resize', setLayout);
+
+	window.addEventListener('scroll', () => {
+		scrollLoop();
+	});
+
+
+})();
